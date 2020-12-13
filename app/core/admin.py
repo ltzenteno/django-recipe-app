0fb269fdd5304a13a83d688dd4ae0e889b56f536
Luis Zenteno <ltzenteno@gmail.com>
2020-12-13 15:28:48 -0500

    modify core admin to support changing custom user model

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 
 from core import models
 
@@ -9,6 +10,18 @@ class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
 
+    # to make the user change page work with our custom model
+    # each tuple is a section
+    fieldsets = (
+        # title, fields
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('Important Dates'), {'fields': ('last_login',)}),
+    )
+
 
 admin.site.register(models.User, UserAdmin)
-
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -35,3 +35,14 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """
+        Test that the user edit page works (since we are using a custom user model) # noqa: E501
+        usually we don't need to test this pages since they are Django framework related # noqa: E501
+        """
+        # creating the url like: /admin/core/user/:id
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
