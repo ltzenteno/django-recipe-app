e81b51bfb1f2f33503f40d0b2fcf1f58c10ea7c9
Luis Zenteno <ltzenteno@gmail.com>
2021-01-04 21:26:34 -0500

    manage user endpoint and tests

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -26,6 +26,18 @@ class UserSerializer(serializers.ModelSerializer):
         """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)  # removing the password from the validated_data # noqa: E501
+        user = super().update(instance, validated_data)
+
+        # set the password
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user authentication object"""
